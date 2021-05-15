3b958461bf0162ebc5b433c0be29b871860ae2f1
twukss <teogoulois@gmail.com>
2021-05-15 10:35:01 +0200

    load all file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React, {useState} from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Input from './screens/input'
+import Leaflet from './screens/leaflet'
+import Dashboard from './screens/dashboard'
 
 function App() {
+  
+  const [ip, setIp] = useState(["retrieving data", "retrieving data", "retrieving data", 'retrieving data']);
+  const [ location, setLocation ] = useState(['lat', 'lng'])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    
+    <Router>
+      <div className="App">
+
+        <div className="bg-top">
+
+          <h1 className="title">IP Address Tracker</h1>
+
+          <div className="test">
+             <Input setIp={setIp} setLocation={setLocation} />
+          </div>
+        </div>
+        
+ 
+        <div className="bg-bottom">
+          <Dashboard ip={ip} location={location} />
+         <Leaflet location={location}  />
+        </div> 
+        
+      </div>
+    </Router>
   );
 }
 
